f187b1fd9b73e758889c3d23225d786cb64f7cea
Jonathan Chavarria <jhon.chavaria@gmail.com>
2019-08-18 18:50:57 -0600

    Design library and initial images and xml

--- app/src/main/java/com/android/androidarchitecture/PodCastListActivity.kt
+++ app/src/main/java/com/android/androidarchitecture/PodCastListActivity.kt
@@ -3,7 +3,7 @@ package com.android.androidarchitecture
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PodCastListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
